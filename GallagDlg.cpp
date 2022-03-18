df7916c55e95cf9eef5c86841c58b1f2f91601d1
ddoddodododdo <rkdtks6128@gmail.com>
2022-03-18 18:14:35 +0900

    Add Player Move

--- GallagDlg.cpp
+++ GallagDlg.cpp
@@ -54,6 +54,13 @@ CGallagDlg::CGallagDlg(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_GALLAG_DIALOG, pParent)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+
+	player.posX = 100;
+	player.posY = 100;
+	player.sizeX = 50;
+	player.sizeY = 50;
+
+
 }
 
 void CGallagDlg::DoDataExchange(CDataExchange* pDX)
@@ -65,6 +72,7 @@ BEGIN_MESSAGE_MAP(CGallagDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_WM_TIMER()
 END_MESSAGE_MAP()
 
 
@@ -117,14 +125,88 @@ void CGallagDlg::OnSysCommand(UINT nID, LPARAM lParam)
 	}
 }
 
+void CGallagDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	Invalidate();
+}
+
+BOOL CGallagDlg::PreTranslateMessage(MSG* pMsg) {
+	//AfxMessageBox(TEXT("눌림"));
+
+	if (pMsg->message == WM_KEYDOWN) {
+		switch (pMsg->wParam)
+		{
+			case VK_LEFT:
+				InputKey.isLeft = true;
+				break;
+			case VK_RIGHT:
+				InputKey.isRight = true;
+				break;
+			case VK_UP:
+				InputKey.isUp = true;
+				break;
+			case VK_DOWN:
+				InputKey.isDown = true;
+				break;
+			default:
+				break;
+		}
+	}
+
+	if (pMsg->message == WM_KEYUP) {
+		switch (pMsg->wParam)
+		{
+			case VK_LEFT:
+				InputKey.isLeft = false;
+				break;
+			case VK_RIGHT:
+				InputKey.isRight = false;
+				break;
+			case VK_UP:
+				InputKey.isUp = false;
+				break;
+			case VK_DOWN:
+				InputKey.isDown = false;
+				break;
+			default:
+				break;
+		}
+	}
+	return CDialogEx::PreTranslateMessage(pMsg);
+}
+
+
 // 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
 //  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
 //  프레임워크에서 이 작업을 자동으로 수행합니다.
 
 void CGallagDlg::OnPaint()
 {
-	
+	CPaintDC dc(this);
+
+	ControllPlayer();
+
+	DrawObject(dc);
+	SetTimer(1000, 1000 / 60, NULL);
 }
+
+void CGallagDlg::ControllPlayer() {
+	double moveX = (InputKey.isRight - InputKey.isLeft) * PLAYER_SPEED;
+	double moveY = (InputKey.isDown - InputKey.isUp) * PLAYER_SPEED;
+
+	player.posX += moveX;
+	player.posY += moveY;
+
+}
+
+void CGallagDlg::DrawObject(CPaintDC& dc) {
+
+	//Draw Player
+	dc.Rectangle(player.posX - player.sizeX * 0.5, player.posY - player.sizeY * 0.5
+		, player.posX + player.sizeX * 0.5, player.posY + player.sizeY * 0.5);
+};
+
+
 
 HCURSOR CGallagDlg::OnQueryDragIcon()
 {
--- GallagDlg.h
+++ GallagDlg.h
@@ -1,4 +1,4 @@
-﻿
+﻿	
 // GallagDlg.h: 헤더 파일
 //
 
@@ -12,23 +12,52 @@ class CGallagDlg : public CDialogEx
 public:
 	CGallagDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
 
+	class GameObj {
+		public:
+			double posX;
+			double posY;
+			double sizeX;
+			double sizeY;
+	};
+
+	class InputKeyClass {
+		public:
+			bool isUp = false;
+			bool isDown = false;
+			bool isLeft = false;
+			bool isRight = false;
+	};
+
+	const double PLAYER_SPEED = 5.0;
+
+	InputKeyClass InputKey;
+	GameObj player;
+
+	
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_GALLAG_DIALOG };
 #endif
-
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
-
+	virtual void DoDataExchange(CDataExchange* pDX);
 
 // 구현입니다.
 protected:
 	HICON m_hIcon;
 
 	// 생성된 메시지 맵 함수
+	void ControllPlayer();
+
+	void DrawObject(CPaintDC& dc);
+
+	BOOL PreTranslateMessage(MSG* pMsg);
+
+
 	virtual BOOL OnInitDialog();
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
 };
